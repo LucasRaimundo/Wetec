7f1805d939395d16ab7015873fdf8e80e2d9b0d7
LucasRaimundo <lucasraimundo2010@gmail.com>
2021-10-19 15:37:16 -0300

    Adicional de sysout

--- Wetec/src/br/com/sistema/Principal.java
+++ Wetec/src/br/com/sistema/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Bem vindo à nossa plataforma Wetec");
 		System.out.println("A plataforma que te ajudará a alcançar seus objetivos");
-
+		System.out.println("Teste de commit");
 	}
 
 }
