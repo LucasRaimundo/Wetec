6db7bd0a232fb2956ee92015670d402509bf771a
LucasRaimundo <lucasraimundo2010@gmail.com>
2021-10-20 15:39:07 -0300

    Adicionando firulas ao sistema principal

--- Wetec/src/br/com/sistema/Principal.java
+++ Wetec/src/br/com/sistema/Principal.java
@@ -3,9 +3,11 @@ package br.com.sistema;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("===================    WETEC    ======================");
 		System.out.println("Bem vindo à nossa plataforma Wetec");
 		System.out.println("A plataforma que te ajudará a alcançar seus objetivos");
-		System.out.println("Teste de commit");
+		System.out.println("======================================================");
+		
 	}
 
 }
